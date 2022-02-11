d23b9d980d7eea1a2186975203f33ece63b72bf0
nguyenhuyhoang250620 <85165266+nguyenhuyhoang250620@users.noreply.github.com>
2022-02-11 10:44:15 +0700

    1

--- backend/api/controllers/ProductsController.js
+++ backend/api/controllers/ProductsController.js
@@ -9,12 +9,17 @@ const table = 'products'
 module.exports = {
     get: (req, res) => {
         let start = (req.params.page-1)*2;
-        let sql = 'SELECT * FROM products LIMIT '+start+',2';
-        db.query(sql, (err, response) => {
-            if (err) throw err
-            res.json(response)
-        })
-        console.log(start)
+        var query = 'SELECT COUNT(*) as total FROM products'
+        db.query(query, function(err, rows, fields) {
+            if (err) throw err;
+            let total = Math.ceil(rows[0].total/2);
+            let sql = 'SELECT * FROM products LIMIT '+start+',2';
+            db.query(sql, (err, response) => {
+                if (err) throw err
+                res.json({response,total})
+            })
+        });
+        
     },
     detail: (req, res) => {
         let sql = 'SELECT * FROM products WHERE id = ?'
